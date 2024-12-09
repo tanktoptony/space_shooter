f2648a0d6871f6f91f048d1a1662ebe364a0ab28
tanktoptony <anthony.jarina@gmail.com>
2024-12-09 11:33:05 -0600

    added movement and window collision. meteor imported

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 running = True
+clock = pygame.time.Clock()
 
 #surface
 surf = pygame.Surface((100, 200))
@@ -22,9 +23,21 @@ x = 100
 
 # importing an image
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+player_direction = pygame.math.Vector2(1, 1)
+player_speed = 300
+
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]# main game loop
+
+meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
+meteor_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+
+laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT -20))
+
 while running:
+    dt = clock.tick() / 1000
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -32,10 +45,20 @@ while running:
 
     # draw the game
     display_surface.fill('darkgray')
-    x += 0.1
-    display_surface.blit(player_surf, (x, 150))
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
-    pygame.display.flip()
+
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+
+    # player movement and window collision
+    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
+        player_direction.y *= -1
+    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
+        player_direction.x *= -1
+    player_rect.center += player_direction * player_speed * dt
+    display_surface.blit(player_surf, player_rect)
+
+    pygame.display.update()
 
 pygame.quit()
